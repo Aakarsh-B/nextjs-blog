a42c13235345c5b62d5a891ed6a62ce7e518f4d6
Aakarsh B <aakarshbiju@gmail.com>
2023-04-07 10:02:25 +0530

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'aakarsh'
+export const siteTitle = 'aakarsh - designer'
 
 export default function Layout({ children, home }) {
   return (
